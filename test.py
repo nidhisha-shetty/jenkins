5d30943eb75b882fd0e0bb198e71abbbe2bfa4e3
Nidhisha Shetty <shetty3491@gmail.com>
2020-12-30 17:02:04 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,41 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-desired_cap = {
- 'browserName': 'iPhone',
- 'device': 'iPhone 11',
- 'realMobile': 'true',
- 'os_version': '14.0',
- 'name': 'BStack-[Python] Sample Test', # test name
- 'build': 'BStack Build Number 1' # CI/CD job or build name
+import os
+username = os.getenv("BROWSERSTACK_USERNAME")
+access_key = os.getenv("BROWSERSTACK_ACCESS_KEY")
+build_name = os.getenv("BROWSERSTACK_BUILD_NAME")
+# browserstack_local = os.getenv("BROWSERSTACK_LOCAL")
+# browserstack_local_identifier = os.getenv("BROWSERSTACK_LOCAL_IDENTIFIER")
+
+caps = {
+ 'os': 'Windows',
+ 'os_version': '10',
+ 'browser': 'chrome',
+ 'browser_version': 'latest',
+ 'name': 'BStack-[Jenkins] Sample Test', # test name
+ 'build': build_name, # CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
+ # 'browserstack.local': browserstack_local,
+ # 'browserstack.localIdentifier': browserstack_local_identifier,
+ 'browserstack.user': username,
+ 'browserstack.key': access_key
 }
+
 driver = webdriver.Remote(
-    command_executor='https://nidhishashetty1:wsyX9aQHCR44pEu7ujzN@hub-cloud.browserstack.com/wd/hub',
-    desired_capabilities=desired_cap)
+    command_executor='https://hub-cloud.browserstack.com/wd/hub',
+    desired_capabilities=caps)
+
+# desired_cap = {
+#  'browserName': 'iPhone',
+#  'device': 'iPhone 11',
+#  'realMobile': 'true',
+#  'os_version': '14.0',
+#  'name': 'BStack-[Python] Sample Test', # test name
+#  # 'build': 'BStack Build Number 1' # CI/CD job or build name
+# }
+# driver = webdriver.Remote(
+#     command_executor='https://nidhishashetty1:wsyX9aQHCR44pEu7ujzN@hub-cloud.browserstack.com/wd/hub',
+#     desired_capabilities=desired_cap)
 driver.get("https://www.google.com")
 if not "Google" in driver.title:
     raise Exception("Unable to load google page!")
